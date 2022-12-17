2d3532b05164f705cee8853922ed7edfc30c33ec
alvaroveloz <alvaro.veloz@gmail.com>
2022-12-16 21:01:37 -0500

    IOET - Created register class

---
+++ classes/Register.py
@@ -0,0 +1,17 @@
+from typing import List
+from Schedule import Schedule
+from User import User
+
+class Register:
+    user_register: User
+    schedule: List[Schedule]
+
+    def __init__(self, user_register: User, schedule: List[Schedule]) -> None:
+        self.user_register = user_register
+        self.schedule = schedule
+
+    def getPairs():
+        pass
+
+    def addRegister():
+        pass
