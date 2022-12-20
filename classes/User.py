bc4005b92ea0b89ef07f091449349e50cc36739f
alvaroveloz <alvaro.veloz@gmail.com>
2022-12-19 20:47:46 -0500

    IOET -  Added docstring to User Class

--- classes/User.py
+++ classes/User.py
@@ -1,4 +1,10 @@
 class User:
+    """
+        User class to handle users.
+    
+        Parameters:
+        name (str): Name of the user  
+    """
     name: str
 
     def __init__(self, name: str) -> None:
